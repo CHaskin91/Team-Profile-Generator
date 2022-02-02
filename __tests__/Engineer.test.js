d36e0105097ab61334c187fd80b437a2c075ced3
Chubbs91 <chaskin1991@outlook.com>
2022-02-02 15:23:17 -0600

    adding Enginner test.  next extend engineer to employee class(accidentally doing this backwards)

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,8 @@
 const Engineer = require('../lib/Engineer.js');
+
+test('creates engineer object', () => {
+    const engineer = new Engineer('Annie');
+  
+    expect(engineer.name).toBe('Annie');
+    expect(engineer.name).toEqual(expect.any(String));
+  });
