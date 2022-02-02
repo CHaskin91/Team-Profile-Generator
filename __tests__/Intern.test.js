d4ccb5a3fc797d9bf8358196efbe1cd3cef14dca
Chubbs91 <chaskin1991@outlook.com>
2022-02-02 16:11:34 -0600

    test intern set-up.  adding intern.js(similar to engineer.js)

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,8 @@
 const Intern = require('../lib/Intern.js');
+
+test('creates intern object', () => {
+    const intern = new Intern('Annie');
+  
+    expect(intern.name).toBe('Annie');
+    expect(intern.name).toEqual(expect.any(String));
+  });
