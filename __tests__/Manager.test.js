b5dca90e11dc3ac6c5a12b92e91712414934e702
Chubbs91 <chaskin1991@outlook.com>
2022-02-02 16:26:02 -0600

    setup manager class.  test is passing.  replaced school/github with Office Number for Manager Class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,8 @@
 const Manager = require('../lib/Manager.js');
+
+test('creates manager object', () => {
+    const manager = new Manager('Annie');
+  
+    expect(manager.name).toBe('Annie');
+    expect(manager.name).toEqual(expect.any(String));
+  });
