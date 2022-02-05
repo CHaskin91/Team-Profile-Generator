0f77e7784316a6ee22df538bd422b95f6f45b057
Chubbs91 <chaskin1991@outlook.com>
2022-02-05 15:51:18 -0600

    manager card is being created now! Finish Intern/Engineers next

--- src/page-template.js
+++ src/page-template.js
@@ -30,5 +30,40 @@ const generateManagerCards = employeeArr => {
 };
 
 module.exports = templateData => {
+    // Destructuring the page data
+    let { managers, engineers, interns } = templateData;
+    console.log(managers); console.log(engineers); console.log(interns);
 
+if (!engineers && !interns) {
+    showCards = `${generateManagerCards(managers)}`
 }
+
+return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <header class = "jumbotron">
+        <h1 class = "display-3">My Team</h1>
+    </header>
+
+    <div class = "container">
+        <div class = "row">
+            <div class = "team-area col-12 d-flex justify-content-center">
+                ${showCards}
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+`;
+};
