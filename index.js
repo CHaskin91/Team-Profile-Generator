7a6849daf8760b69fc84b5ee52bd1ae052f69332
Chubbs91 <chaskin1991@outlook.com>
2022-02-04 19:00:41 -0600

    added first question prompt for index.js

--- index.js
+++ index.js
@@ -4,3 +4,31 @@ const fs = require('fs');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+
+// const generatePage = require('./src/page-template.js');
+
+const teamMemberArr = [];
+
+// Prompt manager with questions
+const promptManager = managerData => {
+    if (!teamMemberArr.managers) {
+        teamMemberArr.managers = [];
+    }
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name.');
+                    return false;
+                }
+            }
+        },
+    ])
+}
+
+promptManager();
